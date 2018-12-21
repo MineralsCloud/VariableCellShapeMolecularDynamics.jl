b08c84938e2ebd255231324294c168e6b29d04aa
Qi Zhang <singularitti@outlook.com>
2018-12-21 01:52:53 -0500

    Deprecate the recursive implementation

--- src/Integrators.jl
+++ src/Integrators.jl
@@ -20,14 +20,6 @@ function velocity_verlet(x::Float64, v::Float64, f::Function, dt::Float64, atomi
 end
 
 function velocity_verlet(x::Float64, v::Float64, f::Function, dt::Float64, atomic_mass::Float64, steps_amount::Int)
-    if steps_amount == 0
-        return x, v
-    else
-        velocity_verlet(velocity_verlet(x, v, f, dt, atomic_mass)..., f, dt, atomic_mass, steps_amount - 1)
-    end
-end
-
-function velocity_verlet(x::Float64, v::Float64, f::Function, dt::Float64, atomic_mass::Float64, steps_amount::Int)
     for i in 1:steps_amount
         x, v = velocity_verlet(x, v, f, dt, atomic_mass)
     end
