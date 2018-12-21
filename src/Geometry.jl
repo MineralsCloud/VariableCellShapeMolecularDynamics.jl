02669b5630a5461bda2c2ee8feb1e0733ab79495
Qi Zhang <singularitti@outlook.com>
2018-12-21 09:42:33 -0500

    Add `Cell` struct in Geometry.jl
    and some methods for it

--- src/Geometry.jl
+++ src/Geometry.jl
@@ -11,10 +11,17 @@ julia>
 """
 module Geometry
 
-using StaticArrays: FieldVector
+using LinearAlgebra: det
+
+using StaticArrays: FieldVector, SMatrix
 
 export Point,
-    Point2D, Point3D
+    Point2D, Point3D,
+    Cell,
+    volume,
+    metric_tensor,
+    inverse_metric_tensor,
+    reciprocalcell
 
 abstract type Point{N, Float64} <: FieldVector{N, Float64} end
 
@@ -28,5 +35,15 @@ struct Point3D <: Point{3, Float64}
     y::Float64
     z::Float64
 end
+
+const Cell = SMatrix{3, 3, Float64}
+
+volume(c::Cell)::Float64 = (abs ∘ det)(c)
+
+metric_tensor(c::Cell) = transpose(c) * c
+
+inverse_metric_tensor(c::Cell) = (inv ∘ metric_tensor)(c)
+
+reciprocalcell(c::Cell) = 2π * (transpose ∘ inv)(c)
 
 end
